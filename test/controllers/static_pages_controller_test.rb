8a53ff66bd2312db43d9856a9767b11c38d441d3
taka.usu <liverpenguin1408@gmail.com>
2020-03-18 11:47:18 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,45 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @baase_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
+  #rootルーティングに対するテスト
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
   test "should get home" do
+    # Homeページのテスト 
+    # GETリクエストをhomeアクションに対して送信
     get static_pages_home_url
     assert_response :success
+    #セレクタ "title" の値のテスト
+    assert_select "title", "Home | #{@baase_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    #セレクタ "title" の値のテスト
+    assert_select "title", "Help | #{@baase_title}"
+  end
+  
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    #セレクタ "title" の値のテスト
+    assert_select "title", "About | #{@baase_title}"
+  end
+  
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    #セレクタ "title" の値のテスト
+    assert_select "title", "Contact | #{@baase_title}"
   end
 
 end
