17f3a25aa6ffe61c840c61293da027c8a11dec9a
taka.usu <liverpenguin1408@gmail.com>
2020-03-22 06:44:53 +0000

    Make a basic User model (inclede secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   
-  #get 'static_pages/home'
-  #get 'static_pages/help'
-  #get 'static_pages/about'
-  #get 'static_pages/contact'
   root 'static_pages#home' #static_pages/home   というURLへアクセスする
   
   get '/help', to:'static_pages#help'
