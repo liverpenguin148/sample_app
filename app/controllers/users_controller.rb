a9b4009ddfe8ccba9ebaaa1c963eb1216986712b
taka.usu <liverpenguin1408@gmail.com>
2020-03-27 13:16:41 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # @userによって、該当するuser_idのページへ遷移
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  #StrongParameters
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
---
+++ test/integration/uses_signup_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class UsesSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do
+    #getメソッドでユーザー登録ページにアクセス
+    get signup_path
+    
+    # postメソッドを使用した（フォーム送信テスト）
+    # assert_no_difference 'User.count' User.countの値がブロック実行前後で変わらないことテスト
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" }}
+    end
+    # 指定されたテンプレートが選択されたか
+    assert_template 'users/new'
+    
+    # formタグ内に/signupがあれば、成功
+    assert_select 'form[action="/signup"]'
+    
+    # id = error_explanationが描画されているかテスト
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    # post送信後、1ユーザーが増えたかどうかテスト
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: {name: "Example User",
+                                        email: "user@example.com",
+                                        password:              "password",
+                                        password_confirmation: "password"}}
+    follow_redirect!                                        
+    assert_template 'users/show'
+    assert_not flash.empty?
+    end
+  end
+end
